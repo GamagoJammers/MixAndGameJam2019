15e460ea3644aeaf482e44a29052f824de444101
aduporge <adrienvduporge@gmail.com>
2019-10-13 16:43:14 +0200

    LinkUpdated

---
+++ Assets/Shaders/Materialize.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Materialize : MonoBehaviour
+{
+	private bool active = false;
+	private float value = 1;
+	public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (!GameManager.instance.isPaused)
+		{
+			if (value > -0.2 && active)
+			{
+				value -= Time.deltaTime * speed;
+			}
+
+			foreach (var material in GetComponent<Renderer>().materials)
+			{
+				material.SetFloat("_Materialize_Amount", value);
+			}
+		}
+	}
+
+	private void OnEnable()
+	{
+		active = true;
+	}
+
+	private void OnDisable()
+	{
+		value = 1;
+	}
+}
